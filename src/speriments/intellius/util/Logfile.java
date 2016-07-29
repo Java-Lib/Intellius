4bbcaadad41fe674141d5f3ad0f9f7042eb36997
Michael <github@codedsoup.com>
2016-07-29 11:58:02 -0700

    Created a logger for Intellius

---
+++ src/speriments/intellius/util/Logfile.java
@@ -0,0 +1,29 @@
+package speriments.intellius.util;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import speriments.intellius.main.Intellius;
+
+public class Logfile {
+	public static final Logger logger = Logger.getLogger("Minecraft");
+	public Logfile() {
+		throw new AssertionError();
+	}
+	
+	public static void log(Level level, String message, boolean raw) {
+		logger.log(level, (raw ? "" : "[" + Intellius.PROJECT + "]: ") + message);
+	}
+	
+	public static void info(String message) {
+		log(Level.INFO, message, false);
+	}
+	
+	public static void warning(String message) {
+		log(Level.WARNING, message, false);
+	}
+	
+	public static void severe(String message) {
+		log(Level.SEVERE, message, false);
+	}
+}
