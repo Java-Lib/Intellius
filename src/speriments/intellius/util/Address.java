46b49ab53b56e52fbdea9371dddeb33a3f316b1c
Michael <github@codedsoup.com>
2016-07-29 14:06:28 -0700

    Created an Address-getter for Intellius

---
+++ src/speriments/intellius/util/Address.java
@@ -0,0 +1,23 @@
+package speriments.intellius.util;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerLoginEvent;
+
+public class Address {
+	public static Address networkData;
+	public static Address getNetworkData() {
+		return networkData;
+	}
+	
+	public String getUserIp(Player player) {
+		return player.getAddress().getAddress().getHostAddress().trim();
+	}
+	
+	public String getUserIp(PlayerLoginEvent event) {
+		return event.getAddress().getHostAddress().trim();
+	}
+	
+	public Integer getUserPort(Player player) {
+		return player.getAddress().getPort();
+	}
+}
