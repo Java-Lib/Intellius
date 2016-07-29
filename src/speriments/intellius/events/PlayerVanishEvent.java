34ddf71bdbcfa7f967566c09fb712ef195378357
Michael <github@codedsoup.com>
2016-07-29 13:47:28 -0700

    Created a Vanish event for Intellius

---
+++ src/speriments/intellius/events/PlayerVanishEvent.java
@@ -0,0 +1,36 @@
+package speriments.intellius.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+
+public class PlayerVanishEvent {
+	public Player player;
+	public String vanish;
+	
+	public PlayerVanishEvent(Player player, String vanish) {
+		this.player = player;
+		this.vanish = vanish;
+	}
+	
+	public Player getPlayer() {
+		return player;
+	}
+	
+	public static void setVanish(Player player, boolean trueOrFalse) {
+		if (trueOrFalse == true) {
+			player.hidePlayer(player);
+		} else {
+			player.showPlayer(player);
+		}
+	}
+	
+	private static final HandlerList handlers = new HandlerList();
+	 
+	public HandlerList getHandlers() {
+	    return handlers;
+	}
+	 
+	public static HandlerList getHandlerList() {
+	    return handlers;
+	}
+}
