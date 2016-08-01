bf4895642a7ab69166dbf7b5350c99f99cad9c69
CodeError <CodeErrorOfficial@users.noreply.github.com>
2016-08-01 14:34:18 -0400

    Update PlayerKillEvent.java

--- src/speriments/intellius/events/PlayerKillEvent.java
+++ src/speriments/intellius/events/PlayerKillEvent.java
@@ -4,8 +4,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 
 public class PlayerKillEvent {
-  public Player targetPlayer;
-  public Player sender;
+  	
+  	public Player targetPlayer;
+  	public Player sender;
 	
 	public PlayerKillEvent(Player player, Player sender) {
 		this.targetPlayer = targetPlayer;
@@ -17,19 +18,19 @@ public class PlayerKillEvent {
 	}
 	
 	public void killPlayer(Player targetPlayer, Player sender) {
-    if (!(sender instanceof Player)) {
-      sender.sendMessage(ChatColor.RED + "Only players can kill other players!");
-      return true;
-    }
+    		if (!(sender instanceof Player)) {
+      			sender.sendMessage(ChatColor.RED + "Only players can kill other players!");
+      			return true;
+    		}
     
-    if (target == null) {
-      sender.sendMessage(ChatColor.RED + targetPlayer + " is not currently online.");
-      return true;
-    } else {
-      targetPlayer.setHealth(0.0D);
-      sender.sendMessage(ChatColor.GREEN + targetPlayer + " has been killed!");
-      return true; 
-    }
+    		if (target == null) {
+      			sender.sendMessage(ChatColor.RED + targetPlayer + " is not currently online.");
+      			return true;
+    		} else {
+      			targetPlayer.setHealth(0.0D);
+      			sender.sendMessage(ChatColor.GREEN + targetPlayer + " has been killed!");
+      			return true; 
+    		}
 	}
 	
 	private static final HandlerList handlers = new HandlerList();
